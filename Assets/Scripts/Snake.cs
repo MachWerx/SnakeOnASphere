14aa74d07dbf72741e40eb7393cd2a194f86137a
Mach Kobayashi <machwerx@gmail.com>
2021-04-23 23:31:04 -0700

    Steerable snake

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -5,20 +5,35 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     private float m_Speed;
+    private float m_RotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_Speed = 0.1f;
+        m_Speed = 0.5f;
+        m_RotationSpeed = 90f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos += m_Speed * transform.up * Time.deltaTime;
-        pos = 0.5f * pos.normalized;
-        transform.position = pos;
-        transform.rotation *= Quaternion.FromToRotation(transform.forward, -pos.normalized);
+        //if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 pos = transform.position;
+            pos += m_Speed * transform.up * Time.deltaTime;
+            pos = 0.5f * pos.normalized;
+            transform.position = pos;
+            //transform.rotation *= Quaternion.FromToRotation(transform.forward, -pos.normalized);
+            transform.rotation = Quaternion.LookRotation(-pos, transform.up);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.AngleAxis(m_RotationSpeed * Time.deltaTime, transform.forward) * transform.rotation;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.AngleAxis(-m_RotationSpeed * Time.deltaTime, transform.forward) * transform.rotation;
+        }
     }
 }
