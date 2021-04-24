7770b3668927df5d1dd776aa83fc2a418e45c779
Mach Kobayashi <machwerx@gmail.com>
2021-04-23 22:36:25 -0700

    Added snake on a sphere

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform m_Snake;
+
+    private float m_DistanceFromCenter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_DistanceFromCenter = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = m_Snake.position.normalized * m_DistanceFromCenter;
+        transform.rotation = Quaternion.LookRotation(m_Snake.forward, m_Snake.up);
+    }
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private float m_Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Speed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos += m_Speed * transform.up * Time.deltaTime;
+        pos = 0.5f * pos.normalized;
+        transform.position = pos;
+        transform.rotation *= Quaternion.FromToRotation(transform.forward, -pos.normalized);
+    }
+}
